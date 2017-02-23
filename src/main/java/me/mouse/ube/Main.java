c4a462e15f6eed467a00ababb9127a811f9d3e46
Mouse <937279156@qq.com>
2017-02-23 18:00:53 +0800

    初始化

---
+++ src/main/java/me/mouse/ube/Main.java
@@ -0,0 +1,18 @@
+package me.mouse.ube;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO 自动生成的方法存根
+		
+	}
+
+}
